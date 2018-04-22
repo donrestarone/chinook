158aa342e937ad7eb76297a2dc8c8694260072f7
donrestarone <shashikejayatunge@gmail.com>
2018-04-22 15:03:38 -0400

    added associations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,7 @@ class Artist < ApplicationRecord
 	
 	has_many :albums
 	has_many :tracks, through: :albums
+	has_many :media_types, through: :tracks
 	
 
 	validates :name, presence: true
--- app/models/track.rb
+++ app/models/track.rb
@@ -2,7 +2,9 @@ class Track < ApplicationRecord
 	
 	belongs_to :album
 	belongs_to :artist
-	#belongs_to :genre
+	belongs_to :genre
+	belongs_to :media_type
+	has_and_belongs_to_many :playlists
 
 
 	SHORT = 180000
