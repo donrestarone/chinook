e6d812b1d57fbf0b9449b7337e8184d82fa21a91
donrestarone <shashikejayatunge@gmail.com>
2018-04-23 19:52:38 -0400

    added through associations to playlists

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,6 @@ class Playlist < ApplicationRecord
 	has_and_belongs_to_many :tracks
 	has_many :genres, through: :tracks 
 	has_many :media_types, through: :tracks
+	has_many :albums, through: :track
+
 end
